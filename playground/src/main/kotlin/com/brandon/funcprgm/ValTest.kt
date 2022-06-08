32646aca8c77515b622c5d008160b953087a5745
eager20 <eager20@gmail.com>
2022-06-09 00:44:39 +0900

    브랜든 수정사항~

--- playground/src/main/kotlin/com/brandon/funcprgm/ValTest.kt
+++ playground/src/main/kotlin/com/brandon/funcprgm/ValTest.kt
@@ -14,4 +14,10 @@ fun main() {
     // 타입은 앞쪽 : 로 선언하든, 뒤쪽에 선언하든 해야함!
 
     println("${multi_newLine(3,4)}")
+
+    val greet = {println("hello")} // 추론가능
+    val square = {x: Int -> x*x} // square 의 자료형을 생략하려면 x의 자료형을 명시해야함.
+
+    val nestedLamda = {{ println("nested")}} // 추론 가능.
+    val nestedLamda2: () -> () -> Unit = {{println("nested")}} // 추론 가능.
 }
